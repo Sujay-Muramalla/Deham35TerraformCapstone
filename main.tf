4d8e048101dbf23b1a67361667cc5428251387b9
Kreyno93 <michael.wagener@neuefische.de>
2026-04-08 17:22:58 +0200

    adding first public subnet with internet connection

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "aws_vpc" "wordpress-tf-vpc" {
     Name = "wordpress-tf-vpc"
   }
 }
+resource "aws_subnet" "public-tf-wordpress-subnet" {
+  vpc_id     = aws_vpc.wordpress-tf-vpc.id
+  cidr_block = "192.168.0.0/28"
+
+  tags = {
+    Name = "public-tf-wordpress-subnet"
+  }
+}
