c44aa8c9b3c877b58f5b5d42f29ef0b269a7ea11
Kreyno93 <michael.wagener@neuefische.de>
2026-04-08 17:25:32 +0200

    adding security group for ec2 instance with ingress rules for ssh, http and mysql and default egress rule

--- main.tf
+++ main.tf
@@ -66,3 +66,43 @@ resource "aws_internet_gateway" "wordpress-tf-igw" {
     Name = "wordpress-tf-igw"
   }
 }
+
+resource "aws_security_group" "wordpress-tf-sg" {
+  name        = "wordpress-tf-sg"
+  description = "Allow for HTTP and SSH access to the WordPress instance"
+  vpc_id      = aws_vpc.wordpress-tf-vpc.id
+
+  tags = {
+    Name = "wordpress-tf-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wordpress-tf-sg-allow-http" {
+  security_group_id = aws_security_group.wordpress-tf-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wordpress-tf-sg-allow-ssh" {
+  security_group_id = aws_security_group.wordpress-tf-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wordpress-tf-sg-allow-mysql" {
+  security_group_id = aws_security_group.wordpress-tf-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+
+resource "aws_vpc_security_group_egress_rule" "wordpress-tf-sg-allow-all" {
+  security_group_id = aws_security_group.wordpress-tf-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
