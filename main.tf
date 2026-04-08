b09c707a92c832c40a90c34a2dc276e62f0cf82c
Kreyno93 <michael.wagener@neuefische.de>
2026-04-08 17:23:36 +0200

    adding route table for public routing

--- main.tf
+++ main.tf
@@ -30,3 +30,16 @@ resource "aws_subnet" "private-tf-wordpress-subnet" {
     Name = "private-tf-wordpress-subnet"
   }
 }
+
+resource "aws_route_table" "public-tf-wordpress-rtb" {
+  vpc_id = aws_vpc.wordpress-tf-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.wordpress-tf-igw.id
+  }
+
+  tags = {
+    Name = "public-tf-wordpress-rtb"
+  }
+}
