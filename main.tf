8c72e0c1ce4152539aaa1f4e3ff4b8e5ab7c215f
Kreyno93 <michael.wagener@neuefische.de>
2026-04-08 17:23:16 +0200

    adding first private subnet for later use

--- main.tf
+++ main.tf
@@ -21,3 +21,12 @@ resource "aws_subnet" "public-tf-wordpress-subnet" {
     Name = "public-tf-wordpress-subnet"
   }
 }
+
+resource "aws_subnet" "private-tf-wordpress-subnet" {
+  vpc_id     = aws_vpc.wordpress-tf-vpc.id
+  cidr_block = "192.168.0.16/28"
+
+  tags = {
+    Name = "private-tf-wordpress-subnet"
+  }
+}
