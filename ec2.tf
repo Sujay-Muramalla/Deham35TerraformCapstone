39b96a016dd6124b29626132efdddf5f5aff064e
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-04-09 12:47:27 +0200

    changed key pair for creation of ec2 instances with free tier account

--- ec2.tf
+++ ec2.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "bastion-tf-instance" {
   ami                         = data.aws_ssm_parameter.amazon_linux_2023.value
   instance_type               = "t3.small" # hardcoded | variables
   associate_public_ip_address = true
-  key_name                    = "vockey"
+  key_name                    = "YAIC-KeyPair"
   subnet_id                   = aws_subnet.public-tf-wordpress-subnet.id
   vpc_security_group_ids      = [aws_security_group.wordpress-tf-sg.id]
   user_data                   = file("scripts/userdata.sh")
